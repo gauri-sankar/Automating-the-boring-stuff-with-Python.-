370d73ccdc4a5a5c4ebe1e59eb8199e685d07194
Gauri Sankar <96428549+gauri-sankar@users.noreply.github.com>
2024-09-04 15:35:47 +0530

    Update printTable.py

--- Chapter 6/printTable.py
+++ Chapter 6/printTable.py
@@ -1,8 +1,14 @@
+#! python3
+#printTable.py - To print the values in a list of string list in the form of a table that is right justified.
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
-
-for row in tableData:
-    for field in row:
-        print(field.rjust(10),end='')
-    print()
+maxlength = 0
+def printTable(table,maxlength=0):
+    colWidth = [0]*len(table)
+    for row in table:
+        maxlength = max(max(len(row[i])  for i in range(len(row))),maxlength)#find the longest column in the row
+        for field in row:
+            print(field.rjust(maxlength+1),end='')
+        print()
+printTable(tableData)
